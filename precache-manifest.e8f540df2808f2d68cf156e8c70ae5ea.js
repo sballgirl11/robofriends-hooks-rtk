c32e96487f737af6d339050386937b08cf6a53c5
Brittney <brittney.postma@hotmail.com>
2020-05-17 17:49:42 -0400

    Updates

---
+++ precache-manifest.e8f540df2808f2d68cf156e8c70ae5ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c3f8e3b592bb48077eb1c07a4eb1ac3",
+    "url": "/robofriends-hooks-rtk/index.html"
+  },
+  {
+    "revision": "0ada70a54969e5c839ba",
+    "url": "/robofriends-hooks-rtk/static/css/main.dec1edb8.chunk.css"
+  },
+  {
+    "revision": "cf6e2da4ef05ec343f97",
+    "url": "/robofriends-hooks-rtk/static/js/2.88b92626.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/robofriends-hooks-rtk/static/js/2.88b92626.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ada70a54969e5c839ba",
+    "url": "/robofriends-hooks-rtk/static/js/main.36843809.chunk.js"
+  },
+  {
+    "revision": "91d96245c605cd86c8ba",
+    "url": "/robofriends-hooks-rtk/static/js/runtime-main.ea4c8a9b.js"
+  }
+]);
